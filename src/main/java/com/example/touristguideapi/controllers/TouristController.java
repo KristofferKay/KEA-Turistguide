b93d9378d62cc962015f4404e390ea0ac0c07660
Kristoffer Kay <37834268+KristofferKay@users.noreply.github.com>
2024-09-04 06:49:47 +0200

    Fixed name

---
+++ src/main/java/com/example/touristguideapi/controllers/TouristController.java
@@ -0,0 +1,4 @@
+package com.example.touristguideapi.controllers;
+
+public class TouristController {
+}
--- src/test/java/com/example/touristguideapi/touristguideapi/DemoApplicationTests.java
+++ src/test/java/com/example/touristguideapi/touristguideapi/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.touristguideapi;
+package com.example.touristguideapi.touristguideapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
